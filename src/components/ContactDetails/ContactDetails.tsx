eb9f6e65f55a06d12531e10f7bc24b0bae087183
Yaseen YK <cbz0178@gmail.com>
2024-01-10 16:14:39 +0530

    Changes for Hero Page

--- src/components/ContactDetails/ContactDetails.tsx
+++ src/components/ContactDetails/ContactDetails.tsx
@@ -1,26 +1,59 @@
-import {motion} from 'framer-motion'
-const ContactDetails = () => {
-    const variants = {
-        initial:{
-            x:3000,
+import React from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
 
-        },
-        animate:{
-            x:0,
-            transition:{
-                duration: 1,
-            }
-        }
+const ContactDetails: React.FC = () => {
+  const nameRef = React.useRef<HTMLInputElement>(null);
+  const emailRef = React.useRef<HTMLInputElement>(null);
+  const messageRef = React.useRef<HTMLTextAreaElement>(null);
+
+  const variants = {
+    initial: {
+      x: 3000,
+    },
+    animate: {
+      x: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (nameRef.current && emailRef.current && messageRef.current) {
+      const formData = {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        message: messageRef.current.value,
+      };
+      emailjs.send('service_lanm9zj', 'template_dyb1k4x', formData, 'mB56akvK2qStLNadU')
+        .then((response) => {
+          console.log('Email sent successfully:', response);
+        })
+        .catch((error) => {
+          console.error('Email sending failed:', error);
+        });
     }
-  return (
-    <motion.div className="Form" variants={variants} initial='initial' animate='animate'>
-        <h2>Contact Me!</h2>
-      <input type="text" placeholder="Enter your Name"/>
-      <input type="text" placeholder="Enter your Email"/>
-      <textarea name="" id=""></textarea>
-      <button>Submit Query!</button>
-    </motion.div>
-  )
-}
+  };
 
-export default ContactDetails
+  return (
+    <motion.div
+      className="Form"
+      variants={variants}
+      initial="initial"
+      animate="animate"
+    >
+      <h2>Contact Me!</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter your Name" ref={nameRef} />
+        <input type="email" placeholder="Enter your Email" ref={emailRef} />
+        <textarea placeholder="Enter your Message" ref={messageRef}></textarea>
+        <button type="submit">Submit Query!</button>
+      </form>
+    </motion.div>
+  );
+};
+
+export default ContactDetails;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,32 @@
-import './Navbar.scss';
-import Github from '../../assets/github.svg';
-import Linkedin from '../../assets/linkedin.svg';
-import Sidebar from '../SIdebar/Sidebar';
+import "./Navbar.scss";
+import Github from "../../assets/github.svg";
+import Linkedin from "../../assets/linkedin.svg";
+import Sidebar from "../SIdebar/Sidebar";
+import Resume from '../../Resume/Resume.pdf';
 const Navbar = () => {
+  const handleDownload = () => {
+    const downloadLink = document.createElement("a");
+
+    downloadLink.download = "Resume.pdf";
+    downloadLink.href = Resume;
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+    document.body.removeChild(downloadLink);
+  };
   return (
-    <div className='navbar'>
-      
-      <div className='wrapper'>
-      <Sidebar/>
-        <span>Yaseen YK</span>
-        <div className='Socials'>
-            <img src={Github} alt='Github'/>
-            <img src={Linkedin} alt='Linkedin'/>
+    <div className="navbar">
+      <div className="wrapper">
+        <Sidebar />
+        <div className="Socials">
+        <button onClick={handleDownload}>Download CV</button>
+          <img src={Github} alt="Github" />
+          <img src={Linkedin} alt="Linkedin" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
